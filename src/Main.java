1aa1f0de92f6ba88d7de6f79114d6b5eb0538fbc
Ze <zeesoze@gmail.com>
2019-12-03 21:10:10 +0000

    Menu Inicial

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Teste");
+		
+		System.out.println("#############");
+		System.out.println("1 - Somar");
+		System.out.println("2 - Subtrair");
+		System.out.println("3 - Multiplicar");
+		System.out.println("4 - Dividir");
+		System.out.println("#############");
+
 	}
+	
+	
+
+	
 
 }
