1321b72167c6b2ad414f4082ad9a548bbaf85a11
TarcBruno <tarcisiob40@gmail.com>
2019-12-03 21:56:20 +0000

    Programa correu bem

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Teste");
-		
+
 		System.out.println("#############");
 		System.out.println("1 - Somar");
 		System.out.println("2 - Subtrair");
@@ -12,10 +12,13 @@ public class Main {
 		System.out.println("4 - Dividir");
 		System.out.println("#############");
 
-	}
-	
-	
+		Utilities calculadora = new Utilities();
 
-	
+		calculadora.Operation();
+
+	}
+
+
+
 
 }
