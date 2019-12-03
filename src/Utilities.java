4f9793b208dfe6dded6ddaeca98315636c018cfe
TarcBruno <tarcisiob40@gmail.com>
2019-12-03 21:30:47 +0000

    Utilities completo

--- src/Utilities.java
+++ src/Utilities.java
@@ -5,23 +5,69 @@ public class Utilities {
 	private double firstValue;
 	private double secondValue;
 
-	public double sum(double firstValue, double secondValue){
+	private double soma(double firstValue, double secondValue){
 
-		return 0;
+		return firstValue + secondValue;
 	}
 
-	public void chooseOperation(){
+	private double subtrair(double firstValue, double secondValue){
+
+		return firstValue - secondValue;
+	}
+
+	private double multiplicar(double firstValue, double secondValue){
+
+		return firstValue * secondValue;
+	}
+
+	private double dividir(double firstValue, double secondValue){
+
+		if(secondValue==0){
+			System.out.println("Numero invalido");
+			return -1;
+		}
+		return firstValue / secondValue;
+	}
+
+	public void Operation(){
 		Scanner scanner = new Scanner(System.in);
 		int option = scanner.nextInt();
 		scanner.nextLine();
+
+		double firstValue, secondValue;
+
 		switch(option){
 
-		case 1:
-			System.out.println("Digite dois numeros para somar: ");
-			double firstValue = scanner.nextDouble();
-			double secondValue = scanner.nextDouble();
-			System.out.println("Total: "+sum(firstValue, secondValue));
+			case 1:
+				System.out.println("Digite dois numeros para somar: ");
+				firstValue = scanner.nextDouble();
+				secondValue = scanner.nextDouble();
+				System.out.println("Total: "+soma(firstValue, secondValue));
+				break;
+
+			case 2:
+				System.out.println("Digite dois numeros para subtrair: ");
+				firstValue = scanner.nextDouble();
+				secondValue = scanner.nextDouble();
+				System.out.println("Total: "+subtrair(firstValue, secondValue));
+				break;
+
+			case 3:
+				System.out.println("Digite dois numeros para multiplicar: ");
+				firstValue = scanner.nextDouble();
+				secondValue = scanner.nextDouble();
+				System.out.println("Total: "+multiplicar(firstValue, secondValue));
+				break;
+
+			case 4:
+				System.out.println("Digite dois numeros para dividir: ");
+				firstValue = scanner.nextDouble();
+				secondValue = scanner.nextDouble();
+				System.out.println("Total: "+dividir(firstValue, secondValue));
+				break;
+
 		}
+
 	}
 
 }
